9db9cd1c068288fd1287e498ce8237672ffd42a5
MichaelNchor <manchor127@gmail.com>
2023-01-12 16:54:15 +0000

    Connected to Phpmyadmin Sqlserver

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
+require("dotenv").config();
+const con = require(__dirname + "/config/database.js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/views/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let items = [];
 app.get("/", function (req, res) {
   let day = date.getDate();
-  res.render("index", { date: day, newListItems: items });
+  let sql = `SELECT * FROM todos`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render("index", { date: day, newListItems: result });
+  });
 });
 
 app.post("/", function (req, res) {
   item = req.body.newItem;
-  items.push(item);
+  let sql = `INSERT INTO todos (item) VALUES ("${item}")`;
+  if (item !== null) {
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("item inserted");
+    });
+  }
   res.redirect("/");
 });
 
-app.listen(3000, function () {
-  console.log("Server running on port: 3000");
+app.put("/", function (req, res) {
+  itemid = req.body.id;
+  itemchecked = req.body.checked;
+  console.log(itemchecked);
+  let sql = `UPDATE todos SET checked="${itemchecked}" WHERE id="${itemid}";`;
+  console.log(sql);
+  if (itemid !== null) {
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("item updated");
+    });
+  }
+});
+
+app.listen(process.env.APP_PORT, function () {
+  console.log("Server running on port: " + process.env.APP_PORT);
 });
---
+++ config/database.js
@@ -0,0 +1,11 @@
+const { createConnection } = require("mysql");
+
+const con = createConnection({
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
+});
+
+module.exports = con;
